4a1c0ca13716d820a80b6c121f713558cf360bb5
Nicolas Santiano <2015081507@ust-ics.mygbiz.com>
2018-09-14 13:50:13 +0800

    Initial commit

---
+++ app/src/main/java/nicolas/santiano/com/santianonicolaslab3/MainActivity.java
@@ -0,0 +1,13 @@
+package nicolas.santiano.com.santianonicolaslab3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
